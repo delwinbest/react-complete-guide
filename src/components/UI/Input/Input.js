f8f5e4e578fcf79e945d7f38dac7faf078d75d37
Delwin Best <mail@delwinbest.com>
2021-05-28 14:10:03 +0900

    create reusable input component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './input.module.css';
+
+export const Input = props => {
+    return <div
+    className={`${classes.control} ${
+      props.isValid === false ? classes.invalid : ''
+    }`}
+  >
+    <label htmlFor={props.id}>{props.label}</label>
+    <input
+      type={props.type}
+      id={props.id}
+      value={props.value}
+      onChange={props.onChange}
+      onBlur={props.oBlur}
+    />
+  </div>
+}
+
+export default Input;
