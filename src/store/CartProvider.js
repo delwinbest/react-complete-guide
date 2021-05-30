cade033fb1c74b4523a9ee902677eef574c16b00
Delwin Best <mail@delwinbest.com>
2021-05-30 11:17:33 +0900

    Add Context Provider for Cart and consume by HeaderCartButton

---
+++ src/store/CartProvider.js
@@ -0,0 +1,20 @@
+import React from "react";
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+  const removeItemFromCart = (id) => {};
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCart,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
