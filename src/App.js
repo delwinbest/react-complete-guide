9181336b26e5313cac95e4930f6cda62699261c4
Delwin Best <mail@delwinbest.com>
2021-05-30 10:53:29 +0900

    Pass Modal show handler via prop chain

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = (params) => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = (params) => {
+    setCartIsShown(false);
+  };
+
   return (
-    <React.Fragment>
-      <Cart />
-      <Header />
+    <Fragment>
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler} />
       <main className="">
         <Meals />
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
