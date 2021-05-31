868f724a680888a290922ca7e29bd091105b0675
Delwin Best <dbest@amazon.co.jp>
2021-05-31 16:56:15 +0900

    AddMovie function and UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -39,19 +40,29 @@ function App() {
     return () => {};
   }, [fetchMoviesHandler]);
 
-  let content = <p>Found 0 Movies</p>;
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
+
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
+
   if (error) {
     content = <p>{error}</p>;
   }
 
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
