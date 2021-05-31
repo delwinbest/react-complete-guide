e32351a4de3adca4482dd6cece2ece1459081eee
Delwin Best <dbest@amazon.co.jp>
2021-05-31 16:26:45 +0900

    using async function and await instead of .then for promises.

--- src/App.js
+++ src/App.js
@@ -5,26 +5,21 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMoviesHandler = (params) => {
-    fetch("https://swapi.dev/api/films", {
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films", {
       method: "GET",
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            release: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      })
-      .catch();
-  };
+    });
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        release: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+  }
 
   useEffect(() => {
     fetchMoviesHandler();
