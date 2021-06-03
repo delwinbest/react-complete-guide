f5a86af97f6eb35ab6fa2d4698f1c89e326fd642
Delwin Best <mail@delwinbest.com>
2021-06-03 19:57:12 +0900

    using http hook multiple places

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,42 +1,37 @@
-import { useState } from "react";
-
+import { useCallback } from "react";
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-complete-guide-3008c-default-rtdb.firebaseio.com/tasks.json",
+  const { onAddTask } = { ...props };
+  const createTask = useCallback(
+    (taskText, data) => {
+      const generatedId = data.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+      onAddTask(createdTask);
+    },
+    [onAddTask]
+  );
+
+  const enterTaskHandler = useCallback(
+    (taskText) => {
+      sendTaskRequest(
         {
+          url: "https://react-complete-guide-3008c-default-rtdb.firebaseio.com/tasks.json",
           method: "POST",
-          body: JSON.stringify({ text: taskText }),
+          body: { text: taskText },
           headers: {
             "Content-Type": "application/json",
           },
-        }
+        },
+        createTask.bind(null, taskText)
       );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
+    },
+    [createTask, sendTaskRequest]
+  );
 
   return (
     <Section>
