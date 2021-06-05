da7112c7b1fe8d7a7cfc2fb34862c074cc9477b4
Delwin Best <mail@delwinbest.com>
2021-06-05 09:29:04 +0900

    handling Loading and Error with useEffect async function

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,12 +5,18 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-complete-guide-3008c-default-rtdb.firebaseio.com/meals.json"
+        "https://react-complete-guide-3008c-default-rtdb.firebaseio.com/meals"
       );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
       const loadedMeals = Object.keys(responseData).map((key) => {
         return {
@@ -20,12 +26,33 @@ const AvailableMeals = () => {
         };
       });
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals()
+      .then()
+      .catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
     return () => {};
   }, []);
 
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
