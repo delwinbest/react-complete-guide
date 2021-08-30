bb3a8589e242dceb38037f5ef6f4a9e58ebc2563
Delwin Best <mail@delwinbest.com>
2021-08-30 10:22:50 +0900

    started cart slice

---
+++ src/store/cart-slice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { action } from 'commander';
+
+createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemToCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        
+      } else {
+        existingItem.quantity 
+      }
+    },
+  },
+});
