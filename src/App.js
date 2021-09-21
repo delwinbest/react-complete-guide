e52d4ad6d0c9a8acecc41ab5da74ea25f318fa78
Delwin Best <mail@delwinbest.com>
2021-09-21 16:43:23 +0900

    have reducers perform logic and in turn dispatch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -15,44 +15,13 @@ function App() {
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        'https://react-tutorial-99024-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
-        { method: 'PUT', body: JSON.stringify(cart) }
-      );
-      if (!response.ok) {
-        throw new Error('Sending cart failed');
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success...',
-          message: 'Sent cart data successfully!',
-        })
-      );
-      // const responseData = await response.json();
-    };
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error...',
-          message: 'Sending Cart Failed',
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
+
   return (
     <Fragment>
       {notification && (
