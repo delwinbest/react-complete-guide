5a07d49f3a38509c7a09ff54b20562887c730307
Delwin Best <mail@delwinbest.com>
2021-09-22 14:29:06 +0900

    dynamic NavLinks

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,22 @@
 import { useParams } from 'react-router';
 import { Route } from 'react-router';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Delwin', text: 'Quote 01' },
+  { id: 'q2', author: 'Delwin B', text: 'Quote 02' },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    return <p>No Quote Found!</p>;
+  }
   return (
     <section>
-      <h1>QuoteDetail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
