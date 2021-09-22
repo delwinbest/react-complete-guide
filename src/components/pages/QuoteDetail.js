0128736676e4a2247e0a6d385bca21636221f0ce
Delwin Best <mail@delwinbest.com>
2021-09-22 11:01:10 +0900

    add nested route

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,5 +1,18 @@
-const QuoteDetail = (params) => {
-  return <h1>QuoteDetail Page</h1>;
+import { useParams } from 'react-router';
+import { Route } from 'react-router';
+import Comments from '../comments/Comments';
+
+const QuoteDetail = () => {
+  const params = useParams();
+  return (
+    <section>
+      <h1>QuoteDetail Page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </section>
+  );
 };
 
 export default QuoteDetail;
